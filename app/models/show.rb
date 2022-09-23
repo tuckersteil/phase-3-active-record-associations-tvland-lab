1f53c3403b7f2c4f62d0ab0497185d102d98ee26
Tucker Steil <tuckersteil@gmail.com>
2022-09-23 11:26:37 -0700

    commit tree 2c149e32848dc4417686de1e645f5e60f87be51b

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list 
+    self.characters.map do |role|
+        "#{role.actor.first_name} #{role.actor.last_name}"
+    end 
+  end 
+
 end
